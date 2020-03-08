1f5f3024cba2f1f8969c157a0d3b295ba2074829
DanBedford1 <61879214+DanBedford1@users.noreply.github.com>
2020-03-08 14:17:41 +0000

    Add files via upload

---
+++ UserPaddle.py
@@ -0,0 +1,32 @@
+import pygame
+# colours
+black = (0, 0, 0)
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, colour, width, height):
+        # Call sprite constructor
+        super().__init__()
+
+        # Pass paddle attributes
+        self.image = pygame.Surface([width, height])
+        self.image.fill(black)
+        self.image.set_colorkey(black)
+
+        # Draw paddle
+        pygame.draw.rect(self.image, colour, [0, 0, width, height])
+        # Get image
+        self.rect = self.image.get_rect()
+
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+        # Check paddle is on screen
+        if self.rect.x > 582:
+            self.rect.x = 582
+
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels
+        # Check paddle is on screen
+        if self.rect.x < 0:
+            self.rect.x = 0
+
+
